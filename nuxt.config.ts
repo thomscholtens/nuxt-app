13cf78ec53df77e47abb7581873023b792e9042a
Thom Scholtens <thom@arlanet.com>
2022-06-28 22:27:55 +0200

    added host to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 module.exports = defineNuxtConfig({
+    server: {
+        host: '0.0.0.0'
+    }
 
 })
