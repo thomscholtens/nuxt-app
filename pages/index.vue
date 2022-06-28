3065dad4f013832653949067214e7c7a7ffaa3df
Thom Scholtens <thom@arlanet.com>
2022-06-29 00:13:28 +0200

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
     <h1>
-        Home page
+        Home pagee
     </h1>
     <p>Lorem ipsum dolor sit amet. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla odio recusandae dolor placeat, vel ipsum sit nostrum nisi temporibus unde.</p>
 </div>
