d9e254292e4571664605d36f15ec486339116942
Thom Scholtens <thom@arlanet.com>
2022-06-28 23:26:33 +0200

    added port to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 module.exports = defineNuxtConfig({
     server: {
-        host: '0.0.0.0'
+        host: '0.0.0.0',
+        port: 80
     }
 
 })
